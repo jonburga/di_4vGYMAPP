f17afca31acef075b5ddbaf780c7f48bb16a9f08
Jon burga <jonanderburga@gmail.com>
2023-12-15 10:45:51 +0100

    Creadno las interfaces ActivtyType,Activity,Monitor

---
+++ src/app/shared/activityType.ts
@@ -0,0 +1,13 @@
+export interface ActivityType{
+    id:number,
+    name:Tipo,
+    numbersOfMonitors:number
+}
+export enum Tipo {
+    Bodypump = 'bodypump',
+    Pilates = 'pilates',
+    Scanning='Sacanning'
+  
+
+  }
+   
---
+++ src/app/shared/monitor.ts
@@ -0,0 +1,7 @@
+export interface Monitor{
+    id:number,
+    name:string,
+    email:string,
+    phone:string,
+    photo:string
+}
